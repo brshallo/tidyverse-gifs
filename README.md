92afc1ae5b6ec8e7e7be196ad4c74212a543d62c
brshallo <brshallo@gmail.com>
2020-06-15 20:53:09 -0400

    add files

---
+++ README.md
@@ -0,0 +1,10 @@
+# tidyverse-gifs
+
+Provides an example of how to create a gif using:
+
+flair` --> `xarigan` + `pagedown` --> `magick`
+
+**Steps:**
+
+1. Create slides (e.g. "xarigan-slides-example.Rmd")
+2. Render slides into a gif (e.g. "render-gif.R")
---
+++ render-gif.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(magick)
+
+# create pdf of slides
+pagedown::chrome_print("xarigan-slides-example.Rmd")
+
+# load slides as images
+images <- image_read_pdf("xarigan-slides-example.pdf", density = 100)
+
+# crop, transform images, render to gif
+images %>% 
+  image_crop("0x350") %>% 
+  image_write_gif(here::here("many-model-performance.gif"))
